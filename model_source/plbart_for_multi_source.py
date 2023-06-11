b74bfe8242dc4998924cdada512b553d4a366cdd
udith kavinda <udithkavinda650@gmail.com>
2023-06-11 22:29:46 +0530

    Update plbart_for_multi_source.py

--- model_source/plbart_for_multi_source.py
+++ model_source/plbart_for_multi_source.py
@@ -229,7 +229,7 @@ class PLBartDecoder(PLBartPreTrainedModel):
             combined_attention_mask = _make_causal_mask(
                 input_shape,
                 inputs_embeds.dtype,
-                inputs_embeds.device,
+                device=inputs_embeds.device,
                 past_key_values_length=past_key_values_length,
             )
 
